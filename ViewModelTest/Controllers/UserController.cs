1e48d2d2ed7e24158bd5d1fddfefa9051dce9dba
cash <cash@cashwu.com>
2016-07-30 17:25:32 +0800

    add viewmodel

---
+++ ViewModelTest/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+using ViewModelTest.Models.ViewModels;
+
+namespace ViewModelTest.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModels pageData)
+        {
+            if (pageData.Account == "cash"
+                && pageData.Password == "1234")
+            {
+                pageData.Msg = $"你使用{pageData.Account}登入成功";
+            }
+            else
+            {
+                pageData.Msg = "登入失敗";
+            }
+
+            return View(pageData);
+        }
+    }
+}
---
+++ ViewModelTest/Models/ViewModels/LoginViewModels.cs
@@ -0,0 +1,11 @@
+﻿namespace ViewModelTest.Models.ViewModels
+{
+    public class LoginViewModels
+    {
+        public string Account { get; set; }
+
+        public string Password { get; set; }
+
+        public string Msg { get; set; } 
+    }
+}
